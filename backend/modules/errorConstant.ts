153ef307aa957a8be0cae07d7fd84e5d3981fca7
Anirban Dutta <dutta.an@northeastern.edu>
2024-10-27 17:05:56 -0400

    Update errorConstant.ts
    Change description in errorConstant.ts

--- backend/modules/errorConstant.ts
+++ backend/modules/errorConstant.ts
@@ -1,3 +1,3 @@
-export const INVALID_PARAMS = 'Please enter valid details';
-export const USER_NOT_FOUND = 'Unable to find the user';
+export const INVALID_PARAMS = 'Please enter valid details of user';
+export const USER_NOT_FOUND = 'Unable to find the user details';
 export const NO_ENTRY_FOUND = 'NO ENTRY FOUND';
